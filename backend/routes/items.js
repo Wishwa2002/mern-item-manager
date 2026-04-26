2da30e736e44d25beacf6a7c8db8e516d6257b93
Wishwa Wijayananda <125870317+Wishwa2002@users.noreply.github.com>
2026-04-26 11:07:37 +0530

    Update items.js

--- backend/routes/items.js
+++ backend/routes/items.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Item = require('../models/items.js');
+const Item = require('../models/items');
 
 // GET all items
 router.get('/', async (req, res) => {
